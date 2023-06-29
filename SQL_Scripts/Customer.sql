34f740ab4d2a5c476a42f018f2167459088c3ae2
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-29 10:09:00 +0530

    Update Customer.sql

--- SQL_Scripts/Customer.sql
+++ SQL_Scripts/Customer.sql
@@ -1,1 +1,20 @@
 
+-- create table script
+USE [KTjune23]
+GO
+
+/****** Object: Table [dbo].[Customers] Script Date: 6/29/2023 10:07:54 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customers] (
+    [CustID]  INT          NOT NULL,
+    [Name]    VARCHAR (50) NOT NULL,
+    [Address] VARCHAR (50) NOT NULL,
+    [Email]   VARCHAR (50) NOT NULL,
+    [Mobile]  VARCHAR (15) NOT NULL
+);
+
